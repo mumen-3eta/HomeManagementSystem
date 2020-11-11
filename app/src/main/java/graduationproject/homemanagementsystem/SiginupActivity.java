e86b26ca7d31dbb0b4c44ab8b2b99bdbb50d81ba
Mumen <mumen.3eta@gmail.com>
2020-11-11 13:27:53 +0200

    ..

---
+++ app/src/main/java/graduationproject/homemanagementsystem/SiginupActivity.java
@@ -0,0 +1,53 @@
+package graduationproject.homemanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class SiginupActivity extends AppCompatActivity {
+
+    private LinearLayout drawer_sign;
+    private DrawerLayout drawer_layout;
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siginup);
+
+        drawer_layout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer_sign = (LinearLayout) findViewById(R.id.drawer_sign);
+
+        drawer_sign.setBackgroundColor(R.color.shade);
+    }
+
+    public void ClickMenu(View view){
+        openDrawer(drawer_layout);
+    }
+
+    public void goHome(View view){ redirectActivity(this, MainActivity.class);}
+
+    public void goLogin(View view){ redirectActivity(this, LoginActivity.class);}
+
+    public void goSignUp(View view){ closeDrawer(drawer_layout);}
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity,aClass);
+        activity.startActivity(intent);
+    }
+}
