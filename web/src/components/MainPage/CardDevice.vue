67c4b92802d1295ed13ba19ad6608a5c3d5ddd66
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2020-12-02 14:48:29 +0200

    Fix some code in file card device and device info and add new page for Edit

--- web/src/components/MainPage/CardDevice.vue
+++ web/src/components/MainPage/CardDevice.vue
@@ -5,7 +5,10 @@
         <img alt="" src="@/assets/img/Group 66.png">
         <div class="d-flex justify-content-center align-items-center mt-2">
           <p>{{ list.name }}</p>
-          <button class="btn btn-danger text-light mx-3 my-auto" @click.prevent="DeleteDevice(list)"> Delete</button>
+          <button class="btn btn-danger text-light ml-2 mr-1 my-auto" @click.prevent="DeleteDevice(list)"><i
+              class="fa fa-trash-alt"></i></button>
+          <button class="btn btn-info text-light mr-2 my-auto" @click.prevent="editDevice(list)"><i
+              class="fa fa-edit"></i></button>
         </div>
       </section>
 
@@ -21,9 +24,14 @@ export default {
     list: Object,
   },
   methods: {
-    DeleteDevice(list) {
-      console.log(list);
+    async DeleteDevice(list) {
+      await console.log(list);
       // this.$store.dispatch() // to delete
+    },
+    editDevice(list) {
+      console.log(list.id);
+      let ID = list.id;
+      this.$router.push(`/mainPage/DeviceInfo/${ID}/edit`);
     }
   },
 
