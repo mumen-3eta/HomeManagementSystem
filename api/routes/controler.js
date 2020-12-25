00f1485a425ccaf64210fb5321349558502b6a5a
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2020-12-25 23:06:07 +0200

    created the database tables (user , processor , controler , controler_location , controler_type And the production tabels for the processor and controler ), added the basic routes for them but still needs some handeling and add fixing the server handeling for the user router

---
+++ api/routes/controler.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+// router.use('/', usersRoute);
+// router.use('/type', processorRoute);
+// router.use('/location', controlerRoute);
+
+module.exports = router;
