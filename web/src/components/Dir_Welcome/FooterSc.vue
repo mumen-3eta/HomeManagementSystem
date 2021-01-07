c4fe181f5a90c8015eaefe0f9133c6cdc30f4e31
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2021-01-07 20:58:38 +0200

    finash footer and start with about us section

--- web/src/components/Dir_Welcome/FooterSc.vue
+++ web/src/components/Dir_Welcome/FooterSc.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
     <div class="container__footerSc">
-
+      <div class="footer__title">
+        <p>All rights reserved<sup>&#169;</sup> {{ this.NowYear }}, HMSy</p>
+      </div>
+      <div class="footer__ul">
+        <ul class="footer__list">
+          <li class="footer__item"><a class="footer__link" href=""><i class="fab fa-facebook"></i></a></li>
+          <li class="footer__item"><a class="footer__link" href=""><i class="fab fa-twitter"></i></a></li>
+          <li class="footer__item"><a class="footer__link" href=""><i class="fab fa-instagram"></i></a></li>
+          <li class="footer__item"><a class="footer__link" href=""><i class="fab fa-github"></i></a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "FooterSc"
+  name: "FooterSc",
+  data() {
+    return {
+      NowYear: null
+    }
+  },
+  mounted() {
+    let today = new Date();
+    this.NowYear = today.getFullYear();
+  }
 }
 </script>
 
 <style scoped>
-
+@import "../../assets/css/_footerScStyle.css";
 </style>
