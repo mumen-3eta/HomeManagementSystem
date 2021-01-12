d65b715c1ac5f50ff1ae327b23cce390712c26f0
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2021-01-12 22:28:31 +0200

    update in welcome page , and update main page and add new menu

--- web/src/layout/Main_Layout/HeaderMain.vue
+++ web/src/layout/Main_Layout/HeaderMain.vue
@@ -8,18 +8,177 @@
         <input class="search__input focus--box-shadow" placeholder="Search ..." type="search">
       </form>
       <div class="profile">
-        <button class="profile__button" type="button">
-          <span class="profile__name">Team Project</span>
+        <button id="btn_Profile" class="profile__button" type="button">
+          <span class="profile__name"><i class="fas fa-caret-down"></i>{{ user.basicInfo.userName }}</span>
           <img alt="profile image" class="profile__img" src="@/assets/img/MainHomePage/user.jpg">
         </button>
+        <div id="profile__menu" class="profile__menu">
+          <ul class="profile__menu-ul">
+            <li class="profile__menu-li">
+              <router-link class="profile__menu-link" to="/v2/main/profile">
+                <i class="fas fa-user-alt profile__menu-icon"></i>
+                <p class="profile__menu-nameTitle"> Profile </p>
+              </router-link>
+            </li>
+            <li class="profile__menu-li">
+              <router-link class="profile__menu-link" to="#">
+                <i class="fas fa-bell profile__menu-icon"></i>
+                <p class="profile__menu-nameTitle">notifications </p>
+              </router-link>
+            </li>
+            <li class="profile__menu-li">
+              <router-link class="profile__menu-link" to="#">
+                <i class="fas fa-desktop profile__menu-icon"></i>
+                <p class="profile__menu-nameTitle">my device</p>
+              </router-link>
+            </li>
+            <li class="profile__menu-li" @click.prevent="handleClick">
+              <a class="profile__menu-link" href="javascript:(void(0));">
+                <i class="fas fa-sign-out-alt profile__menu-icon"></i>
+                <p class="profile__menu-nameTitle">log out</p>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </header><!-- End HEADER -->
 </template>
 
 <script>
+import axios from "axios";
+import {mapGetters} from "vuex";
+
 export default {
-  name: "HeaderMain"
+  name: "HeaderMain",
+  mounted() {
+    document.getElementById("btn_Profile").addEventListener("click", function () {
+      document.getElementById("btn_Profile").classList.toggle("profile__menu-open");
+      document.getElementById("profile__menu").classList.toggle("profile__menu-open");
+    });
+  },
+  methods: {
+    async handleClick() {
+      axios.defaults.headers.common['csrf-token'] = localStorage.getItem('csrfToken');
+      await axios.put('/api/v1/users/logout');
+      localStorage.removeItem('csrfToken');
+      this.$store.dispatch('user', null);
+      this.$store.dispatch('TokenUser', null);
+      this.$store.dispatch('deviceInfoAdd', null);
+      this.$router.push('/v2/login');
+    }
+  },
+  async beforeMount() {
+    axios.defaults.headers.common['csrf-token'] = localStorage.getItem('csrfToken');
+    const {user} = (await axios.get('/api/v1/users/me')).data;
+    await this.$store.dispatch('user', user);
+  },
+  computed: {
+    ...mapGetters(['user', 'deviceInfoAdd', 'TokenUser'])
+  },
 }
 </script>
+
+<style scoped>
+.profile__menu-open {
+  background-color: white;
+  border-radius: 10px 10px 0 0;
+  box-shadow: 1px 1px 4px #9d9c9c;
+  transition: all 0.3s ease;
+}
+
+.profile__menu {
+  position: absolute;
+  width: 100%;
+  height: 12.74rem;
+  background-color: white;
+  z-index: 999;
+  box-shadow: 1px 1px 4px #9d9c9c;
+  border-radius: 0 0 10px 10px;
+  display: none;
+  transition: all 0.3s ease;
+}
+
+.profile__menu-open {
+  display: flex;
+  transition: all 0.3s ease;
+}
+
+@media (max-width: 769px) {
+  .profile__menu {
+    width: 13.65rem;
+    top: 3.71rem;
+    left: -9rem;
+    border-radius: 10px 0 10px 10px;
+  }
+}
+
+i.fa-caret-down {
+  padding-right: 0.5rem;
+}
+
+.profile__menu-ul {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: start;
+}
+
+.profile__menu-li {
+  width: 100%;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: start;
+  padding: 0.5rem;
+}
+
+.profile__menu-li:not(:last-child) {
+  border-bottom: 2px solid #cfcfcf;
+}
+
+.profile__menu-li:last-child {
+  background-color: #cb5959;
+  border-radius: 0 0 10px 10px;
+  color: white;
+  transition: all 0.3s ease;
+}
+
+.profile__menu-li:last-child .profile__menu-link {
+  color: white;
+}
+
+@media (hover: hover) {
+  .profile__menu-li:last-child:hover {
+    background-color: #a93e3e;
+  }
+
+  .profile__menu-li:last-child:hover .profile__menu-link {
+    color: #fff490;
+  }
+}
+
+.profile__menu-link {
+  width: 100%;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: start;
+}
+
+.profile__menu-icon {
+  padding: 0.125rem 1rem;
+  font-size: 0.9rem;
+}
+
+.profile__menu-nameTitle {
+  padding: 0.125rem 1rem;
+  font-size: 1.25rem;
+  font-family: Ubuntu, Arial, Helvetica, sans-serif;
+  text-transform: capitalize;
+}
+
+</style>
 
