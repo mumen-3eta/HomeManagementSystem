5809b6f2edb19d166f2157da4441c52545a679da
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2021-01-13 15:54:01 +0200

    update in welcome page , and update main page and update new menu and Add validation in form send message

--- web/src/components/Dir_Welcome/ContactUsAndInfoSc.vue
+++ web/src/components/Dir_Welcome/ContactUsAndInfoSc.vue
@@ -28,22 +28,26 @@
 
           </div>
           <div v-animate-onscroll.repeat="'animated fadeIn fadeInLeft'" class="contactMsg__Section">
-            <form action="#" class="form__contactUs">
+            <form class="form__contactUs" @submit.prevent="checkSendMessage">
               <div class="form__contactUs-group">
                 <label id="lab_EmailInputContact" class="labelInputContact "
                        for="EmailInputContact">Email</label>
-                <input id="EmailInputContact" class="InputContact" type="email">
+                <input id="EmailInputContact" v-model.trim="Email" class="InputContact" type="email">
+                <p class="error_style mTop__5">{{ errors.errorEmail }}</p>
               </div>
               <div class="form__contactUs-group">
                 <label id="lab_SubjectInputContact" class="labelInputContact" for="SubjectInputContact">Subject</label>
-                <input id="SubjectInputContact" class="InputContact" type="text">
+                <input id="SubjectInputContact" v-model.trim="Subject" class="InputContact" type="text">
+                <p class="error_style mTop__5">{{ errors.errorSubject }}</p>
               </div>
               <div class="form__contactUs-group">
                 <label id="lab_MsgInputContact" class="labelInputContact" for="MsgInputContact">Message</label>
-                <textarea id="MsgInputContact" class="InputContact" cols="30" rows="10"></textarea>
+                <textarea id="MsgInputContact" v-model.trim="Message" class="InputContact" cols="30"
+                          rows="10"></textarea>
+                <p class="error_style">{{ errors.errorMessage }}</p>
               </div>
               <div class="form__contactUs-group">
-                <button class="form__contactUs-btn">Send</button>
+                <button class="form__contactUs-btn" type="submit">Send</button>
               </div>
             </form>
           </div>
@@ -56,6 +60,99 @@
 <script>
 export default {
   name: "ContactUsAndInfoSc",
+  data() {
+    return {
+      Email: null,
+      Subject: null,
+      Message: null,
+      errors: {
+        errorEmail: null,
+        errorSubject: null,
+        errorMessage: null,
+      },
+    }
+  },
+  methods: {
+    checkSendMessage() {
+      if (this.Email) {
+        let email = this.Email;
+        let EmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let CheckEmail = EmailRegex.test(email);//true or false
+        if (!CheckEmail) {
+          this.errors.errorEmail = "Sorry! Email Faild, must be (@) and (.) and ignored space";
+          setTimeout(() => {
+            this.errors.errorEmail = null;
+          }, 3000);
+          return false;
+        }
+      }
+      if (this.Subject) {
+        let numberWord = this.Subject.match(/\w+/g).length;
+        let numberCharacter = this.Subject.length;
+        if (numberCharacter >= 60 || numberCharacter <= 0) {
+          this.errors.errorSubject = "Sorry! Characters must be less than 60 character";
+          setTimeout(() => {
+            this.errors.errorSubject = null;
+          }, 3000);
+          return false;
+        }
+        if (numberWord >= 20 || numberWord <= 0) {
+          this.errors.errorSubject = "Sorry! Words must be less than 10 Word";
+          setTimeout(() => {
+            this.errors.errorSubject = null;
+          }, 3000);
+          return false;
+        }
+      }
+      if (this.Message) {
+        let numberWordMessage = this.Message.match(/\w+/g).length;
+        let numberCharacterMessage = this.Message.length;
+        if (numberCharacterMessage >= 1000 || numberCharacterMessage <= 0) {
+          this.errors.errorMessage = "Sorry! Characters must be less than 1000 character";
+          setTimeout(() => {
+            this.errors.errorMessage = null;
+          }, 3000);
+          return false;
+        }
+        if (numberWordMessage >= 250 || numberWordMessage <= 0) {
+          this.errors.errorMessage = "Sorry! Words must be less than 250 Word";
+          setTimeout(() => {
+            this.errors.errorMessage = null;
+          }, 3000);
+          return false;
+        }
+      }
+      if (this.Email || this.Subject || this.Message) {
+        // section Api for send message
+        // try {
+        //    code..
+        // }catch (e) {
+        //    catch errors..
+        // }
+        this.$swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Thank you, Send it',
+          text: "The message has been sent, you will be contacted in the coming hours on the same email!",
+          showConfirmButton: false,
+          timer: 1500
+        })
+        this.EmptyFrom();//empty form
+      }
+
+    },
+    EmptyFrom() {
+      this.Email = null;
+      this.Subject = null;
+      this.Message = null;
+      document.getElementById("lab_EmailInputContact").style.backgroundColor = "";
+      document.getElementById("lab_EmailInputContact").classList.remove("AddMoved");
+      document.getElementById("lab_SubjectInputContact").style.backgroundColor = "";
+      document.getElementById("lab_SubjectInputContact").classList.remove("AddMoved");
+      document.getElementById("lab_MsgInputContact").style.backgroundColor = "";
+      document.getElementById("lab_MsgInputContact").classList.remove("AddMoved");
+    },
+  },
   mounted() {
     const input1 = document.getElementById("EmailInputContact");
     const labelInput1 = document.getElementById("lab_EmailInputContact");
@@ -67,16 +164,18 @@ export default {
 
     input1.addEventListener("focusin", function () {
       SlidUp(labelInput1, "AddMoved");
+      labelInput1.style.backgroundColor = "#fff";
       SlidUp(input1, "AddBorder");
 
     })
     input2.addEventListener("focusin", function () {
       SlidUp(labelInput2, "AddMoved");
+      labelInput2.style.backgroundColor = "#fff";
       SlidUp(input2, "AddBorder");
     })
     input3.addEventListener("focusin", function () {
-      console.log("fefe")
       SlidUp(labelInput3, "AddMoved");
+      labelInput3.style.backgroundColor = "#fff";
       SlidUp(input3, "AddBorder");
     })
 
@@ -85,18 +184,21 @@ export default {
       if (!input1.value) {
         SlidDown(labelInput1, "AddMoved");
         SlidDown(input1, "AddBorder");
+        labelInput1.style.backgroundColor = "";
       }
     })
     input2.addEventListener("focusout", function () {
       if (!input2.value) {
         SlidDown(labelInput2, "AddMoved");
         SlidDown(input2, "AddBorder");
+        labelInput2.style.backgroundColor = "";
       }
     })
     input3.addEventListener("focusout", function () {
       if (!input3.value) {
         SlidDown(labelInput3, "AddMoved");
         SlidDown(input3, "AddBorder");
+        labelInput3.style.backgroundColor = "";
       }
     })
 
@@ -115,4 +217,10 @@ export default {
 
 <style scoped>
 @import "../../assets/css/_contactUsAndInfoScStyle.css";
+
+.mTop__5 {
+  margin-top: -0.8rem;
+  margin-left: 0.5rem;
+  text-transform: capitalize;
+}
 </style>
