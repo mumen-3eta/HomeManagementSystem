2a723fe63111edd14230061b346ad5af44d5a47f
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2021-03-06 19:37:52 +0200

    update wellcome page and add some code and convert some rebete data to for loop

--- client/web/src/components/Dir_Welcome/AskedQuestionSc.vue
+++ client/web/src/components/Dir_Welcome/AskedQuestionSc.vue
@@ -6,128 +6,23 @@
           <h2 class="asked-h">Frequently Asked Questions</h2>
         </div>
         <div class="askedQuestionSc-body">
-          <div v-animate-css.click="'animated fadeIn'" class="container__body-asked">
-            <div ref="btnDown11" class="container__question" @click.prevent="down1">
-              <p class="text-question">How is a decentralized network different from the traditional web?</p>
-              <button id="btn_Down-01" ref="btnDown01" class="text-question__btn" @click.prevent="down11">
+          <div v-for="(askedQuestion , index) in ListQuestions" :key="index" class="container__body-asked">
+            <div class="container__question" @click.prevent="ChangeStatus(index)">
+              <p class="text-question">{{ askedQuestion.question }}</p>
+              <button v-show="!askedQuestion.show" class="text-question__btn">
                 <i class="fas fa-angle-down"></i>
               </button>
-              <button id="btn_Up-01" class="text-question__btn Change_btn">
+              <button v-show="askedQuestion.show" class="text-question__btn">
                 <i class="fas fa-angle-up"></i>
               </button>
-
             </div>
-            <div id="content_answer-01" class="container__answer ">
+            <div v-show="askedQuestion.show" v-animate-css="'animated fadeIn'" class="container__answerShowed">
               <p class="text-answer">
-                Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to
-                connect to a single server containing that data. This method of storing data makes it easy for third
-                parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple
-                devices in the network, which makes it harder to hack and interfere with your data.
-              </p>
-            </div>
-          </div><!--duplicated -->
-          <div v-animate-css.click="'animated fadeIn'" class="container__body-asked">
-            <div ref="btnDown22" class="container__question" @click.prevent="down2">
-              <p class="text-question">How is a decentralized network different from the traditional web?</p>
-              <button id="btn_Down-02" ref="btnDown02" class="text-question__btn" @click.prevent="down22">
-                <i class="fas fa-angle-down "></i>
-              </button>
-              <button id="btn_Up-02" class="text-question__btn Change_btn">
-                <i class="fas fa-angle-up"></i>
-              </button>
-
-            </div>
-            <div id="content_answer-02" class="container__answer ">
-              <p class="text-answer">
-                Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to
-                connect to a single server containing that data. This method of storing data makes it easy for third
-                parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple
-                devices in the network, which makes it harder to hack and interfere with your data.
-              </p>
-            </div>
-          </div><!--duplicated -->
-          <div v-animate-css.click="'animated fadeIn'" class="container__body-asked">
-            <div ref="btnDown33" class="container__question" @click.prevent="down3">
-              <p class="text-question">How is a decentralized network different from the traditional web?</p>
-              <button id="btn_Down-03" ref="btnDown03" class="text-question__btn" @click.prevent="down33">
-                <i class="fas fa-angle-down "></i>
-              </button>
-              <button id="btn_Up-03" class="text-question__btn Change_btn">
-                <i class="fas fa-angle-up"></i>
-              </button>
-
-            </div>
-            <div id="content_answer-03" class="container__answer ">
-              <p class="text-answer">
-                Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to
-                connect to a single server containing that data. This method of storing data makes it easy for third
-                parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple
-                devices in the network, which makes it harder to hack and interfere with your data.
-              </p>
-            </div>
-          </div><!--duplicated -->
-          <div v-animate-css.click="'animated fadeIn'" class="container__body-asked">
-            <div ref="btnDown44" class="container__question" @click.prevent="down4">
-              <p class="text-question">How is a decentralized network different from the traditional web?</p>
-              <button id="btn_Down-04" ref="btnDown04" class="text-question__btn" @click.prevent="down44">
-                <i class="fas fa-angle-down "></i>
-              </button>
-              <button id="btn_Up-04" class="text-question__btn Change_btn">
-                <i class="fas fa-angle-up"></i>
-              </button>
-
-            </div>
-            <div id="content_answer-04" class="container__answer ">
-              <p class="text-answer">
-                Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to
-                connect to a single server containing that data. This method of storing data makes it easy for third
-                parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple
-                devices in the network, which makes it harder to hack and interfere with your data.
-              </p>
-            </div>
-          </div><!--duplicated -->
-          <div v-animate-css.click="'animated fadeIn'" class="container__body-asked">
-            <div ref="btnDown55" class="container__question" @click.prevent="down5">
-              <p class="text-question">How is a decentralized network different from the traditional web?</p>
-              <button id="btn_Down-05" ref="btnDown05" class="text-question__btn" @click.prevent="down55">
-                <i class="fas fa-angle-down "></i>
-              </button>
-              <button id="btn_Up-05" class="text-question__btn Change_btn">
-                <i class="fas fa-angle-up"></i>
-              </button>
-
-            </div>
-            <div id="content_answer-05" class="container__answer ">
-              <p class="text-answer">
-                Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to
-                connect to a single server containing that data. This method of storing data makes it easy for third
-                parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple
-                devices in the network, which makes it harder to hack and interfere with your data.
-              </p>
-            </div>
-          </div><!--duplicated -->
-          <div v-animate-css.click="'animated fadeIn'" class="container__body-asked">
-            <div ref="btnDown66" class="container__question" @click.prevent="down6">
-              <p class="text-question">How is a decentralized network different from the traditional web?</p>
-              <button id="btn_Down-06" ref="btnDown06" class="text-question__btn" @click.prevent="down66">
-                <i class="fas fa-angle-down "></i>
-              </button>
-              <button id="btn_Up-06" class="text-question__btn Change_btn">
-                <i class="fas fa-angle-up"></i>
-              </button>
-
-            </div>
-            <div id="content_answer-06" class="container__answer ">
-              <p class="text-answer">
-                Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to
-                connect to a single server containing that data. This method of storing data makes it easy for third
-                parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple
-                devices in the network, which makes it harder to hack and interfere with your data.
+                {{ askedQuestion.answer }}
               </p>
             </div>
           </div><!--duplicated -->
         </div>
-
       </div>
     </div>
   </div>
@@ -136,86 +31,42 @@
 <script>
 export default {
   name: "AskedQuestionSc",
+  data() {
+    return {
+      ListQuestions: [
+        {
+          question: "what is HMSy ?",
+          answer: "this is a project created by Student in Islamic University  form Gaza , To complete Graduation Project",
+          show: false,
+        },
+        {
+          question: "How is a decentralized network different from the traditional web?",
+          answer: "Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to connect to a single server containing that data. This method of storing data makes it easy for third parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple devices in the network, which makes it harder to hack and interfere with your data.",
+          show: false,
+        },
+        {
+          question: "How is a decentralized network different from the traditional web?",
+          answer: "Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to connect to a single server containing that data. This method of storing data makes it easy for third parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple devices in the network, which makes it harder to hack and interfere with your data.",
+          show: false,
+        },
+        {
+          question: "How is a decentralized network different from the traditional web?",
+          answer: "Decentralization is a key aspect of PiperNet. Traditionally, to access data on a website, you needed to connect to a single server containing that data. This method of storing data makes it easy for third parties to get access to it, corrupt it or even delete it. In PiperNet it is securely stored on multiple devices in the network, which makes it harder to hack and interfere with your data.",
+          show: false,
+        },
+      ]
+    }
+  },
   methods: {
-    down1() {
-      this.$refs.btnDown01.click()
-    },
-    down11() {
-      this.$refs.btnDown11.click()
-    },
-    down2() {
-      this.$refs.btnDown02.click()
-    },
-    down22() {
-      this.$refs.btnDown22.click()
-    },
-    down3() {
-      this.$refs.btnDown03.click()
-    },
-    down33() {
-      this.$refs.btnDown33.click()
-    },
-    down4() {
-      this.$refs.btnDown04.click()
-    },
-    down44() {
-      this.$refs.btnDown44.click()
-    },
-    down5() {
-      this.$refs.btnDown05.click()
-    },
-    down55() {
-      this.$refs.btnDown55.click()
-    },
-    down6() {
-      this.$refs.btnDown06.click()
-    },
-    down66() {
-      this.$refs.btnDown66.click()
+    // to show and hid content
+    ChangeStatus(numberQuestion) {
+      let ListQuestions = this.$data.ListQuestions;
+      ListQuestions[numberQuestion].show = !ListQuestions[numberQuestion].show;
     },
   },
-  mounted() {
-    // bt01
-    document.getElementById("btn_Down-01").addEventListener("click", function () {
-      document.getElementById("content_answer-01").classList.toggle("hidden_answer");
-      document.getElementById("btn_Down-01").classList.toggle("Change_btn");
-      document.getElementById("btn_Up-01").classList.toggle("Change_btn");
-    })
-    // bt02
-    document.getElementById("btn_Down-02").addEventListener("click", function () {
-      document.getElementById("content_answer-02").classList.toggle("hidden_answer");
-      document.getElementById("btn_Down-02").classList.toggle("Change_btn");
-      document.getElementById("btn_Up-02").classList.toggle("Change_btn");
-    })
-    // bt03
-    document.getElementById("btn_Down-03").addEventListener("click", function () {
-      document.getElementById("content_answer-03").classList.toggle("hidden_answer");
-      document.getElementById("btn_Down-03").classList.toggle("Change_btn");
-      document.getElementById("btn_Up-03").classList.toggle("Change_btn");
-    })
-    // bt04
-    document.getElementById("btn_Down-04").addEventListener("click", function () {
-      document.getElementById("content_answer-04").classList.toggle("hidden_answer");
-      document.getElementById("btn_Down-04").classList.toggle("Change_btn");
-      document.getElementById("btn_Up-04").classList.toggle("Change_btn");
-    })
-    // bt05
-    document.getElementById("btn_Down-05").addEventListener("click", function () {
-      document.getElementById("content_answer-05").classList.toggle("hidden_answer");
-      document.getElementById("btn_Down-05").classList.toggle("Change_btn");
-      document.getElementById("btn_Up-05").classList.toggle("Change_btn");
-    })
-    // bt06
-    document.getElementById("btn_Down-06").addEventListener("click", function () {
-      document.getElementById("content_answer-06").classList.toggle("hidden_answer");
-      document.getElementById("btn_Down-06").classList.toggle("Change_btn");
-      document.getElementById("btn_Up-06").classList.toggle("Change_btn");
-    })
-  }
 }
 </script>
 
-<style scoped >
-/* @import "../../assets/sass/AskedQuestionSc.scss"; */
+<style scoped>
 @import "../../assets/css/_askedQuestionScStyle.css";
 </style>
--- client/web/src/components/Dir_Welcome/OurTeamSc.vue
+++ client/web/src/components/Dir_Welcome/OurTeamSc.vue
@@ -5,187 +5,26 @@
       <div class="main__ourTeamSc">
         <carousel :autoplay="true" :autoplaySpeed="1000" :dots="true" :nav="false"
                   :responsive="{0:{items:1},600:{items:2},1200:{items:3},1800:{items:4}}">
-          <!--      member 1 - osama      -->
-          <div v-animate-onscroll="'animated fadeIn fadeInUp'" class="Member__Team">
+          <div v-for="(MemberTeam , index) in ListProjectTeams" :key="index"
+               v-animate-onscroll="'animated fadeIn fadeInUp'" class="Member__Team">
             <div class="Member__Team-img">
-              <img alt="image team member" class="Member__img"
-                   src="@/assets/img/welcomePage/imgTeam/osama.jpg">
+              <img :alt="MemberTeam.ImagePath" :src="MemberTeam.ImagePath"
+                   class="Member__img">
             </div>
             <div class="Member__Team--group">
-              <h3 class="Member__Team-name">Osama Younis</h3>
-              <p class="Member__Team-job">BackEnd Developer</p>
+              <h3 class="Member__Team-name">{{ MemberTeam.name }}</h3>
+              <p class="Member__Team-job">{{ MemberTeam.job }}</p>
               <div class="Social_media-main">
                 <ul class="Social_media-ul">
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://www.facebook.com/Osama.w.you/" target="_blank">
-                      <i class="fab fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://github.com/Osama-you" target="_blank">
-                      <i class="fab fa-github"></i>
+                  <li v-for="(SocialMedia,i) in MemberTeam.Social_media_list " :key="i" class="Social_media-list">
+                    <a :href="SocialMedia.link" class="Social_media-link" target="_blank">
+                      <i :class="SocialMedia.Type"></i>
                     </a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
-
-          <!--      member 2 - Abo obada     -->
-          <div v-animate-onscroll="'animated fadeIn fadeInUp'" class="Member__Team">
-            <div class="Member__Team-img">
-              <img alt="image team member" class="Member__img"
-                   src="@/assets/img/welcomePage/imgTeam/AboObada.jpg">
-            </div>
-            <div class="Member__Team--group">
-              <h3 class="Member__Team-name">Ahmed Iqtifan</h3>
-              <p class="Member__Team-job">HardWar Developer</p>
-              <div class="Social_media-main">
-                <ul class="Social_media-ul">
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://www.facebook.com/profile.php?id=100003143624009"
-                       target="_blank">
-                      <i class="fab fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-github"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <!--      member 3 - Ahmed qeshta     -->
-          <div v-animate-onscroll="'animated fadeIn fadeInUp'" class="Member__Team">
-            <div class="Member__Team-img">
-              <img alt="image team member" class="Member__img"
-                   src="@/assets/img/welcomePage/imgTeam/A7medQeshta.jpg">
-            </div>
-            <div class="Member__Team--group">
-              <h3 class="Member__Team-name">Ahmed Qeshta</h3>
-              <p class="Member__Team-job">FrontEnd Developer</p>
-              <div class="Social_media-main">
-                <ul class="Social_media-ul">
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://www.facebook.com/A7medQeshta" target="_blank">
-                      <i class="fab fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://github.com/AhmedQeshta" target="_blank">
-                      <i class="fab fa-github"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <!--      member 4 - Alaa    -->
-          <div v-animate-onscroll="'animated fadeIn fadeInUp'" class="Member__Team">
-            <div class="Member__Team-img">
-              <img alt="image team member" class="Member__img"
-                   src="@/assets/img/welcomePage/imgTeam/Alaa.png">
-            </div>
-            <div class="Member__Team--group">
-              <h3 class="Member__Team-name">Alaa Shurrab</h3>
-              <p class="Member__Team-job">BackEnd Developer</p>
-              <div class="Social_media-main">
-                <ul class="Social_media-ul">
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://www.facebook.com/alaa.shurrab.39" target="_blank">
-                      <i class="fab fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://github.com/AlaaShurrab" target="_blank">
-                      <i class="fab fa-github"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <!--  -->
-          <!--   member 5  - Mumen A. Abu Aita    -->
-          <div v-animate-onscroll="'animated fadeIn fadeInUp'" class="Member__Team">
-            <div class="Member__Team-img">
-              <img alt="image team member" class="Member__img"
-                   src="@/assets/img/welcomePage/imgTeam/mumen.jpg">
-            </div>
-            <div class="Member__Team--group">
-              <h3 class="Member__Team-name">Mumen Abu Aita</h3>
-              <p class="Member__Team-job">Android Developer</p>
-              <div class="Social_media-main">
-                <ul class="Social_media-ul">
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://www.facebook.com/Mumen.Aita" target="_blank">
-                      <i class="fab fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="#">
-                      <i class="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li class="Social_media-list">
-                    <a class="Social_media-link" href="https://github.com/mumen-3eta" target="_blank">
-                      <i class="fab fa-github"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
         </carousel>
       </div>
     </div>
@@ -198,6 +37,127 @@ import carousel from "vue-owl-carousel";
 export default {
   name: "OurTeamSc",
   components: {carousel},
+  data() {
+    return {
+      ListProjectTeams: [
+        {
+          ImagePath: require(`@/assets/img/welcomePage/imgTeam/osama.jpg`),
+          name: "Osama Younis",
+          job: "BackEnd Developer",
+          Social_media_list: [
+            {
+              link: "https://www.facebook.com/Osama.w.you/",
+              Type: "fab fa-facebook-f",
+            },
+            {
+              link: "#",
+              Type: "fab fa-twitter",
+            },
+            {
+              link: "#",
+              Type: "fab fa-linkedin-in",
+            },
+            {
+              link: "https://github.com/Osama-you",
+              Type: "fab fa-github",
+            },
+          ],
+        },
+        {
+          ImagePath: require(`@/assets/img/welcomePage/imgTeam/AboObada.jpg`),
+          name: "Ahmed Iqtifan",
+          job: "HardWar Developer",
+          Social_media_list: [
+            {
+              link: "https://www.facebook.com/profile.php?id=100003143624009",
+              Type: "fab fa-facebook-f",
+            },
+            {
+              link: "#",
+              Type: "fab fa-twitter",
+            },
+            {
+              link: "#",
+              Type: "fab fa-linkedin-in",
+            },
+            {
+              link: "#",
+              Type: "fab fa-github",
+            },
+          ],
+        },
+        {
+          ImagePath: require(`@/assets/img/welcomePage/imgTeam/A7medQeshta.jpg`),
+          name: "Ahmed Qeshta",
+          job: "FrontEnd Developer",
+          Social_media_list: [
+            {
+              link: "https://www.facebook.com/A7medQeshta",
+              Type: "fab fa-facebook-f",
+            },
+            {
+              link: "#",
+              Type: "fab fa-twitter",
+            },
+            {
+              link: "#",
+              Type: "fab fa-linkedin-in",
+            },
+            {
+              link: "https://github.com/AhmedQeshta",
+              Type: "fab fa-github",
+            },
+          ],
+        },
+        {
+          ImagePath: require(`@/assets/img/welcomePage/imgTeam/Alaa.png`),
+          name: "Alaa Shurrab",
+          job: "BackEnd Developer",
+          Social_media_list: [
+            {
+              link: "https://www.facebook.com/alaa.shurrab.39",
+              Type: "fab fa-facebook-f",
+            },
+            {
+              link: "#",
+              Type: "fab fa-twitter",
+            },
+            {
+              link: "#",
+              Type: "fab fa-linkedin-in",
+            },
+            {
+              link: "https://github.com/AlaaShurrab",
+              Type: "fab fa-github",
+            },
+          ],
+        },
+        {
+          ImagePath: require(`@/assets/img/welcomePage/imgTeam/mumen.jpg`),
+          name: "Mumen Abu Aita",
+          job: "Android Developer",
+          Social_media_list: [
+            {
+              link: "https://www.facebook.com/Mumen.Aita",
+              Type: "fab fa-facebook-f",
+            },
+            {
+              link: "#",
+              Type: "fab fa-twitter",
+            },
+            {
+              link: "#",
+              Type: "fab fa-linkedin-in",
+            },
+            {
+              link: "https://github.com/mumen-3eta",
+              Type: "fab fa-github",
+            },
+          ],
+        },
+      ],
+    }
+  },
 }
 </script>
 
