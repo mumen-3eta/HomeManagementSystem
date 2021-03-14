8716fd1a560f46d717c72d8a1f7541aead3cb41c
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-03-14 18:43:22 +0200

    adding the log out route

--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -1,12 +1,14 @@
 const addUsers = require('./addUsers');
 const authUser = require('./authUser');
 const login = require('./login');
+const logout = require('./logout')
 const getAllProfileData = require('./getAllProfileData');
 const updateProfileData = require('./updateProfileData');
 
 module.exports = {
   addUsers,
   login,
+  logout,
   authUser,
   getAllProfileData,
   updateProfileData,
