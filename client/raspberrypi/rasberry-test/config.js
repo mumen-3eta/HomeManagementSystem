68a29f502e4f3567184fd17d70aa1899e4354f3f
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-04-18 02:35:49 +0300

    adding testing clint data

---
+++ client/raspberrypi/rasberry-test/config.js
@@ -0,0 +1,10 @@
+var config = {}
+
+config.server_url = "http://localhost:3000";
+
+// if(config.led == -1 || config.server_url == ""){
+//   console.log("Before running app.js you must configure the LED pin number and server url in ./config.js");
+//   process.exit(-1);
+// }
+
+module.exports = config;
